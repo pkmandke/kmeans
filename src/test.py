d4dd8b9007f711c73d79db9baa942a2096788ea3
Prathamesh Mandke <mandkepk97@gmail.com>
2019-10-22 20:12:44 -0400

    (@ rahul: use this commit)print total time taken on stdout instead of using time cmd

--- src/test.py
+++ src/test.py
@@ -27,13 +27,15 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--new_tf', type=bool, default=False)
-    parser.add_argument('--n_idf', type=str, default='2') 
+    parser.add_argument('--n_idf', type=str, default='2')
     parser.add_argument('--n_clus', default=10, type=int) # N/A for CH score compute
     parser.add_argument('--n_km', default='1', type=str) # N/A for CH score compute
     parser.add_argument('--ch_idx', default=True, type=bool)
 
     args = parser.parse_args()
-    
+    print("Start")
+    t_st = time.monotonic()
+
     if args.new_tf:
         print("Computing TFIDF vectors:")
         t1 = time.monotonic()
@@ -44,9 +46,9 @@ def main():
         #tfidf.save(SAVE_PATH + 'TFIDF_' + args.n_idf + '.sav')
     else:
         tfidf = joblib.load(RD_PATH + 'TFIDF_' + args.n_idf + '.sav')
-    
+
     ch_index = dict()
-    
+
     data = tfidf.tfidf_matrix.copy()
     if args.ch_idx:
         for cluster in [5, 10, 12, 15, 20, 25]:
@@ -56,11 +58,11 @@ def main():
             km.fit(data)
             print("Done in {}s".format(timedelta(seconds=time.monotonic() - t1)))
             joblib.dump(km, SAVE_PATH + 'kmeans_' + args.n_idf + '_' + str(cluster) + '.sav')
-            
+
             ch_index[cluster] = km.compute_chi_index(data)
-            
+
             print("CH score is {}".format(ch_index[cluster]))
-        
+
         joblib.dump(ch_index, SAVE_PATH + 'ch_score_dict.sav')
     else:
         km = Kmeans(n_clusters=args.n_clus, doc_list=tfidf.doc_list.copy())
@@ -72,6 +74,8 @@ def main():
         print("Done in {}s".format(timedelta(seconds=time.monotonic() - t1)))
         joblib.dump(km, SAVE_PATH + 'kmeans_' + args.n_idf + '_' + args.n_km + '.sav')
 
+
+    print("Done. Total time taken {}s".format(timedelta(seconds=time.monotonic() - t_st)))
+    
 if __name__ == '__main__':
     main()
-    
